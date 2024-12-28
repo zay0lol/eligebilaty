757b413cac81730eb338401fd9c8d46b6cb95912
zay0lol <zynahisthebest2012@gmail.com>
2024-12-28 22:55:39 +0500

    exam eligebilaty

---
+++ main.py
@@ -0,0 +1,6 @@
+percentage = float(input("Enter your percentage: "))
+
+if percentage > 75:
+    print("You can participate in exams!")
+else:
+    print("You are not allowed to take exams! Please improve your grades")
